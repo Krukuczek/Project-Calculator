cf54e797640b72ca38c928bcd467c3c144c5f911
Krukuczek <kubakruk123321@gmail.com>
2022-05-30 19:05:57 +0200

    add keyboard support

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const posibbleKeys=['0','1','2','3','4','5','6','7','8','9','.','=','Enter','Backspace','+','/','-','*']
 let screenNumber="";
 let lastResult="";
 let result="0";
@@ -7,6 +7,13 @@ const keyboard=document.querySelectorAll(".keyBtn");
 keyboard.forEach(btn=> {
     btn.addEventListener("click",function(e){
         return displayOnScreen(this.id);
+    });
+});
+document.addEventListener("keydown",function(e){
+    posibbleKeys.forEach(element=>{
+        if(element===e.key){
+            return(displayOnScreen(element));
+        };
     });
 });
 
@@ -18,13 +25,23 @@ function displayOnScreen(value){
         case "c":
             screenNumber=screenNumber.slice(0, -2);
             break;
+        case "Backspace":
+            screenNumber=screenNumber.slice(0, -10);
+            break;
         case "ac":
             screenNumber="";
             showResult.innerText="";
             lastResult="";
+            result="0";
             break;
         case "=":
             screenNumber=screenNumber.slice(0, -1);
+            operate(showResult);
+            lastResult=screenNumber;
+            screenNumber="";
+            break;
+        case "Enter":
+            screenNumber=screenNumber.slice(0, -5);
             operate(showResult);
             lastResult=screenNumber;
             screenNumber="";
@@ -53,8 +70,17 @@ function mathIt(screenNumber,lastResult) {
             screenNumber=result+screenNumber;
             case "/":
             screenNumber=result+screenNumber;
+            case "*":
+            screenNumber=result+screenNumber;
         }
         result=eval(screenNumber);
+        if(result===Infinity){
+            alert("don't divide by zero!!");
+            return "0";
+        }else if(result===NaN || result===undefined){
+            alert("i have little problem with that :(")
+            return "0";
+        }
         return result;
         
     }
@@ -66,7 +92,7 @@ function doAFlip(){
     };
     const spinning= [
         { transform: 'rotate(0) scale(0.8)' },
-        { transform: 'rotate(360deg) scale(0.8)' },
+        { transform: 'rotate(360deg) scale(1)' },
       ];
     calculator.animate(spinning,timing); 
 }
